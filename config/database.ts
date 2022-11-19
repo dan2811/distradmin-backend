3e7a3eacfd99964b81883b4edb36f85edb1e7fc9
dan2811 <djordandrums@gmail.com>
2022-11-19 17:26:47 +0000

    alter db config for heroku postgres

--- config/database.ts
+++ config/database.ts
@@ -4,10 +4,10 @@ export default ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', process.env.PROD ? process.env.DATABASE_HOST : '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+      database: env('DATABASE_NAME', process.env.PROD ? process.env.DATABASE_NAME : 'postgres'),
+      user: env('DATABASE_USERNAME', process.env.PROD ? process.env.DATABASE_USERNAME : 'postgres'),
       password: env('DATABASE_PASSWORD', process.env.PROD ? process.env.DATABASE_PASSWORD : 'password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
