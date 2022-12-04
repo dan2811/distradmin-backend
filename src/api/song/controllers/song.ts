accca2879874ec87dd2ad690c9dc646bcb4da371
dan2811 <djordandrums@gmail.com>
2022-12-04 01:30:47 +0000

    implement endpoints for sets

---
+++ src/api/song/controllers/song.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController("api::song.song", {
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+    return convertToDataAndTotal(data, meta);
+  },
+  async count(ctx) {
+    try {
+      const { data, meta } = await super.find(ctx);
+      return convertToDataAndTotal(data, meta);
+    } catch (e) {
+      ctx.body = e;
+      return;
+    }
+  },
+});
+
+const convertToDataAndTotal = (data, meta) => {
+  return { data, total: meta.pagination.total };
+};
