daabc966801858731eb81fa08a0e3b19bb526e46
dan2811 <djordandrums@gmail.com>
2022-12-06 16:46:10 +0000

    add endpoints for package entity

--- src/api/package/controllers/package.ts
+++ src/api/package/controllers/package.ts
@@ -1,7 +1,23 @@
-/**
- * package controller
- */
+"use strict";
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::package.package');
+export default factories.createCoreController("api::package.package", {
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+    return convertToDataAndTotal(data, meta);
+  },
+  async count(ctx) {
+    try {
+      const { data, meta } = await super.find(ctx);
+      return convertToDataAndTotal(data, meta);
+    } catch (e) {
+      ctx.body = e;
+      return;
+    }
+  },
+});
+
+const convertToDataAndTotal = (data, meta) => {
+  return { data, total: meta.pagination.total };
+};
