ab0aad51d5e3e15bb32f39f41e3c3d860cbe4b59
dan2811 <djordandrums@gmail.com>
2022-12-30 17:07:24 +0000

    alter db config

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,14 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', process.env.PROD ? process.env.DATABASE_HOST : 'localhost'),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', process.env.PROD ? process.env.DATABASE_NAME : 'postgres'),
-      user: env('DATABASE_USERNAME', process.env.PROD ? process.env.DATABASE_USERNAME : 'postgres'),
-      password: env('DATABASE_PASSWORD', process.env.PROD ? process.env.DATABASE_PASSWORD : 'password'),
-      ssl: false,
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
   },
 });
