dbc2f6b0f062f0cf11edd690a0d99522bf2eec25
dan2811 <djordandrums@gmail.com>
2023-01-08 17:57:11 +0000

    alter jwt length

--- src/api/musician/controllers/musician.ts
+++ src/api/musician/controllers/musician.ts
@@ -3,21 +3,21 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController("api::musician.musician", {
-  async find(ctx) {
-    const { data, meta } = await super.find(ctx);
-    return convertToDataAndTotal(data, meta);
-  },
-  async count(ctx) {
-    try {
-      const { data, meta } = await super.find(ctx);
-      return convertToDataAndTotal(data, meta);
-    } catch (e) {
-      ctx.body = e;
-      return;
-    }
-  },
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx);
+        return convertToDataAndTotal(data, meta);
+    },
+    async count(ctx) {
+        try {
+            const { data, meta } = await super.find(ctx);
+            return convertToDataAndTotal(data, meta);
+        } catch (e) {
+            ctx.body = e;
+            return;
+        }
+    },
 });
 
 const convertToDataAndTotal = (data, meta) => {
-  return { data, total: meta.pagination.total };
+    return { data, total: meta.pagination.total };
 };
--- src/api/musician/routes/count-musician.ts
+++ src/api/musician/routes/count-musician.ts
@@ -1,12 +1,12 @@
 "use strict";
 
 module.exports = {
-  routes: [
-    {
-      // Path defined with a URL parameter
-      method: "GET",
-      path: "/musicians/count",
-      handler: "musician.count",
-    },
-  ],
+    routes: [
+        {
+            // Path defined with a URL parameter
+            method: "GET",
+            path: "/musicians/count",
+            handler: "musician.count",
+        },
+    ],
 };
