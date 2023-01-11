a14d9da555de6c559948ec6be40b4207c0e85966
dan2811 <djordandrums@gmail.com>
2023-01-11 17:13:09 +0000

    re-instate local dev

--- config/database.ts
+++ config/database.ts
@@ -7,9 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME', process.env.DATABASE_NAME),
       user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
       password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      },
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
