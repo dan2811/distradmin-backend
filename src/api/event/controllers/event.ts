1f3da2ae880fa3a12eec46cdd6efef865a206b70
dan2811 <djordandrums@gmail.com>
2023-01-26 20:44:55 +0000

    delete orphaned jobs on event delete

--- src/api/event/controllers/event.ts
+++ src/api/event/controllers/event.ts
@@ -16,8 +16,37 @@ export default factories.createCoreController("api::event.event", {
       return;
     }
   },
-});
+  async delete(ctx) {
+    const { request } = ctx;
+    const { url } = request;
+    const eventID = url.split('/').pop();
 
+    const jobs = await strapi.entityService.findMany('api::job.job', {
+      populate: {
+        event: true
+      },
+      filters: {
+        event: {
+          id: {
+            $eq: eventID
+          }
+        }
+      },
+    });
+
+    console.log(`Before deleting event ${eventID}, need to delete related jobs. Jobs to delete`, jobs);
+
+    jobs.forEach(async (job) => {
+      const deletedJob = await strapi.entityService.delete("api::job.job", job.id);
+      console.log("deleted job: ", deletedJob);
+    });
+
+    const { data: deletedEvent } = await super.delete(ctx);
+
+    return deletedEvent;
+
+  }
+});
 
 const convertToDataAndTotal = (data, meta) => {
   return { data, total: meta.pagination.total };
