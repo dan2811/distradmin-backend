7489e8edd5d3dfe6a562cac746b3bf2a05eaae14
dan2811 <djordandrums@gmail.com>
2023-03-01 16:44:48 +0000

    use ssl for stage db

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME', process.env.DATABASE_NAME),
       user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
       password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', process.env.NODE_ENV !== "stage" ? true : false),
     },
   },
 });
