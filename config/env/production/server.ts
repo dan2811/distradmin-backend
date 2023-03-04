a04b47b5846ca405ce58e71f981877f459a187b3
danbackup <126896161+danbackup@users.noreply.github.com>
2023-03-04 15:19:24 +0000

    change env vars for server

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,4 +1,9 @@
 export default ({ env }) => ({
-    url: env('STRAPI_DEPLOYMENT_URL', 'https://distradmin-backend.herokuapp.com'),
-    proxy: true,
-});
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    url: process.env.STRAPI_DEPLOYMENT_URL,
+  });
+  
